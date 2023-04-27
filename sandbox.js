cf4991b611a439ed0ab85a90d1752977e7ab6bfe
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-04-27 19:36:34 +0200

    array and objects

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,26 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-character = 'luigi';
-age = 40;
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3) error
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun') error
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30,
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age ='30'; error
+// ninja.skills = ['fighting', 'sneaking']; error
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40,
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,32 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3) error
 
-character = 'luigi';
-age = 40;
-isBlackBelt = true;
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun') error
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30,
 };
 
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age ='30'; error
+// ninja.skills = ['fighting', 'sneaking']; error
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+};
