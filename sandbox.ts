8fb08bcbba568d4633a68a3ce08ebda460511fab
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-04-27 18:03:55 +0200

    type basics

--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,13 @@
-const characater = 'luigi';
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-console.log(characater);
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach((input) => {
-  console.log(input);
-});
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(7.5));
