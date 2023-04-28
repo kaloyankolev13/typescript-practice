82a6e649bb876db945279c9c2b2621ced89fb728
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-04-28 18:20:13 +0200

    Classes

--- public/app.js
+++ public/app.js
@@ -1,13 +1,22 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// console.log(anchor);
-// if (anchor) {
-//   console.log(anchor.href);
-// }
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne, invTwo);
+invOne.client = 'Wario';
+invTwo.amount = 400;
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -15,8 +24,5 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value);
-    console.log(tofrom.value);
-    console.log(details.value);
-    console.log(amount.valueAsNumber);
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
