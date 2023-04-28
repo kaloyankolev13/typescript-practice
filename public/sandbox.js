382c308140eca48677438b828fcf6d222069111d
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2023-04-28 17:26:03 +0200

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('test');
+let greet;
+// greet = 'hello';
+greet = () => {
+    console.log('hello, again!');
+};
+const add = (a, b, c = 2) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
